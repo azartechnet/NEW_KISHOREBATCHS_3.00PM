75e394ed9dd692ff29bf2e36fdc5185828bc2c3d
azartechnet <azartechnet@gmail.com>
2025-10-25 16:14:31 +0530

    Update index.js

--- Reactjs/myapp/src/index.js
+++ Reactjs/myapp/src/index.js
@@ -1,10 +1,49 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const App=()=>{
+/*const App=()=>{
   return(
-    <h1>Welcome to the React Session</h1>
+    <>
+    <h1>Heading-1</h1>
+  <h2>Heading-2</h2>
+    </>
+  
+    
   )
 }
 const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<App/>)
+r1.render(<App/>)*/
+
+//Dynamic Rendering
+
+/*const Sample=()=>{
+  const name="azar";
+  const age=30;
+  return(
+    <div>
+      <h1>Hello{name}</h1>
+      <p>Your age is::{age}</p>
+      <p>Today date is::{new Date().toLocaleDateString()}</p>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>)*/
+
+//Using Login Page
+
+const Login=()=>{
+  return(
+    <>
+    <form>
+      <label>UserName</label>
+      <input type="text"/>
+      <label>Password</label>
+      <input type="text"/>
+      <input type="submit" value="Login"/>
+    </form>
+    </>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Login/>)
