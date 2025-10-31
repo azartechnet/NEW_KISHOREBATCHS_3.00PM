ae3d627c12a5ce292519614d29fd431d7c6ed494
azartechnet <azartechnet@gmail.com>
2025-10-31 16:10:35 +0530

    Update index.js

--- Reactjs/myapp/src/index.js
+++ Reactjs/myapp/src/index.js
@@ -188,7 +188,7 @@ r1.render(<Sample name="azar" age="38"/>)*/
 
 //Component in Component
 
-function Component1()
+/*function Component1()
 {
   return(
     <div>
@@ -204,4 +204,184 @@ function Component2()
   )
 }
 const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<Component1/>)
+r1.render(<Component1/>)*/
+
+//Constructor using props
+
+/*class Sample extends React.Component
+{
+  constructor(props)
+  {
+      super(props);
+      this.state={name:props.name,age:props.age}
+  }
+  render()
+  {
+    return(
+      <div>
+        <h1>Welcome {this.props.name}{this.props.age}</h1>
+      </div>
+    )
+  }
+
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample name="azar" age="25"/>)*/
+
+//Class component another program
+
+/*class Counter extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state={count:0}
+  }
+  increment=()=>{
+    this.setState({count:this.state.count+1})
+  }
+  decrement=()=>{
+    this.setState({count:this.state.count-1})
+  }
+
+  render()
+  {
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    )
+  }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Counter/>)*/
+
+//Another Example
+
+/*class ToggleExample extends React.Component
+{
+  constructor(props){
+  super(props);
+  this.state={visible:false}
+  }
+  toggle=()=>{
+    this.setState({visible:!this.state.visible})
+
+  }
+  render()
+  {
+    return(
+      <>
+       <h1>ToggleExample</h1>
+      <button onClick={this.toggle}>{this.state.visible?'Hide':'Show'}</button>
+      {this.state.visible&&<p>Hello I am visible</p>}
+      </>
+     
+    )
+  }
+ 
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<ToggleExample/>)*/
+
+//React Event
+/*function Football()
+{
+    const shoot=()=>{
+      alert("Goal")
+    }
+    return(
+      <>
+         <button onClick={shoot}>Shoot</button>
+      </>
+    )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Football/>)*/
+
+//React argument passing
+/*function Football()
+{
+  const shoot=(year)=>{
+    alert(`Goal in ${year}`)
+  }
+  return(
+    <>
+       <button onClick={()=>shoot(2025)}>Shoot</button>
+    </>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Football/>)*/
+
+/*function MissedGoal()
+{
+  return<h1>Missed</h1>
+}
+function MadeGoal()
+{
+  return<h1>MadeGoal</h1>
+}
+function Football(props)
+{
+  const isGoal=props.isGoal;
+  if(isGoal)
+  {
+    return<MadeGoal/>
+  }
+  else
+  {
+    return<MissedGoal/>
+  }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(<Football isGoal={true}/>)
+r1.render(<Football isGoal={Math.random()>0.5}/>)*/
+
+//React List using Map
+
+/*function Car(props)
+{
+  return<li>I am {props.b1}</li>
+}
+function Garage()
+{
+  const cars=["BMW","Audi","Toyota"]
+  return(
+    <div>
+      <h1>Garage</h1>
+      <ul>
+        {cars.map((car)=><Car b1={car}/>)}
+      </ul>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Garage/>)*/
+
+//React using key
+
+function Car(props)
+{
+  return<li>{props.id}{props.name}</li>
+}
+function Garage()
+{
+  const cars=[
+    {id:1,name:"BMW"},
+    {id:2,name:"Audi"},
+    {id:3,name:"Toyota"}
+  ]
+  return(
+    <div>
+      <h1>Garage</h1>
+      <ul>
+        {cars.map((car)=><Car id={car.id} name={car.name}/>)}
+      </ul>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Garage/>)
